636ed9f5219338143c43c371f22fff5a423ccdf4
SosikuKawiiHog <razdobarov_se@edu.surgu.ru>
2026-09-04 20:26:52 +0500

    initial

---
+++ vue-gadgets/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MyComponent from './components/MyComponent.vue';
+import MyNewComponent from "@/components/MyNewComponent.vue";
+</script>
+
+<template>
+    <MyComponent></MyComponent>
+    <MyNewComponent></MyNewComponent>
+</template>
+
+<style scoped>
+</style>
