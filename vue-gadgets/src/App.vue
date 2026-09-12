a5305982451ab68de5770d9fff61448204654446
SosikuKawiiHog <razdobarov_se@edu.surgu.ru>
2026-09-12 14:11:00 +0500

    feat: vue components and API logic

--- vue-gadgets/src/App.vue
+++ vue-gadgets/src/App.vue
@@ -1,11 +1,103 @@
 <script setup>
-import MyComponent from './components/MyComponent.vue';
-import MyNewComponent from "@/components/MyNewComponent.vue";
+import ProductList from "@/components/ProductList.vue";
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const items = ref([])
+const fetchItems = async () => {
+    try{
+        const {data} = await axios.get(
+            "https://8554d981576cd888.mokky.dev/items"
+        );
+        items.value = data.map((obj) => ({
+            ...obj,
+            isFavorite: false,
+            isAdded: false,
+        }));
+    } catch(e){
+        console.log(e);
+    }
+};
+onMounted(async () => {
+    await fetchItems();
+});
 </script>
 
 <template>
-    <MyComponent></MyComponent>
-    <MyNewComponent></MyNewComponent>
+    <body class="flex flex-col justify-between h-dvh">
+        <div class="mx-auto w-[1440px] mb-25">
+            <nav class="py-3 border-b border-b-slate-200">
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center gap-3">
+                        <img
+                            src="/logo.svg"
+                            alt=""
+                            />
+                        <p class="text-sm">
+                            Лучшие цены <br/>
+                            в интернет-магазинах
+                        </p>
+                    </div>
+                    <ul class="flex gap-10">
+                        <li>
+                            <a
+                                href=""
+                                class="flex gap-3 items-center"
+                            >
+                                <span class="p-3.5 rounded-x1 bg-slate-100 hover:bg-slate-200 transition"
+                                      >
+                                    <img
+                                        src="/cart-icon.svg"
+                                        alt=""
+                                        />
+                                </span>
+                                Корзина
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href=""
+                                class="flex gap-3 items-center"
+                            >
+                                <span class="p-3.5 rounded-x1 bg-slate-100 hover:bg-slate-200 transition"
+                                >
+                                    <img
+                                        src="/like-outline.svg"
+                                        alt=""
+                                    />
+                                </span>
+                                Избранное
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href=""
+                                class="flex gap-3 items-center"
+                            >
+                                <span class="p-3.5 rounded-x1 bg-slate-100 hover:bg-slate-200 transition"
+                                >
+                                    <img
+                                        src="/orders-icon.svg"
+                                        alt=""
+                                    />
+                                </span>
+                                Заказы
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main class="pt-10">
+                <h1 class="text-[40px] font-bold mb-5">Каталог</h1>
+                <product-list :items="items"/>
+            </main>
+        </div>
+        <footer class="p-6 bg-slate-100">
+            <div class="text-center text-slate-500">
+                Copyright C 2026 Behoof, Inc. Все права нарушены
+            </div>
+        </footer>
+    </body>
 </template>
 
 <style scoped>
---
+++ vue-gadgets/src/components/ProductList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import VProduct from "@/components/VProduct.vue";
+defineProps({
+    items: Array,
+})
+</script>
+
+<template>
+    <div class="grid grid-cols-5 gap-5">
+        <v-product
+            v-for="item in items"
+            :key="item.id"
+            :img-url="item.imageUrl"
+            :title="item.title"
+            :price="item.price.toLocaleString('ru-RU')"></v-product>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
